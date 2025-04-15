5ce8d3cbc07bfeaa6945bbf2499427c1d90ff1b9
My Nameuser.jahonovjavohir0@gmail.com <jahonovjavohir0@gmail.com>
2025-04-15 12:22:25 +0500

    jwt basic configuration

--- src/main/java/uz/pdp/service/JwtService.java
+++ src/main/java/uz/pdp/service/JwtService.java
@@ -7,13 +7,14 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 import uz.pdp.model.User;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
 @Service
 public record JwtService() {
 
-    private static final long accessExpirationAt= 30_000L;
+    private static final long accessExpirationAt= 300_000L;
     private static final String accessSecretKey= "3cfa76ef14937c1c0ea519f8fc057a80fcd04a7420f8e8bcd0a7567c272e007b";
 
     public String generateAccessToken(User user) {
@@ -30,5 +31,15 @@ public record JwtService() {
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(accessSecretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getIdFromToken(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 }
